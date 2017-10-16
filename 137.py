5d2706832a7cbed8ddf68d63c160f4877543352f
Mr. Bartucz <jobartucz@isd535.org>
2017-10-16 08:19:02 -0500

    added stubs for functions

---
+++ 137.py
@@ -0,0 +1,30 @@
+''' Write your names and which function(s) you have completed '''
+
+
+def hangman_display(guessed, secret):
+    ''' Explain what this function does
+    '''
+    
+    
+    return_string = ''
+    
+    return return_string
+
+
+def matches(ticket, winners):
+    ''' Explain what this function does
+    '''
+
+    num_in_common = 0
+    
+    return num_in_common
+
+
+def report(guess, secret):
+    ''' Explain what this function does
+    '''
+
+    num_right_wrong = [0, 0]
+    
+    return num_right_wrong
+
